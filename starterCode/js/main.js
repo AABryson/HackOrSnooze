b6f12824d2b5807a17d65ad4e52eb3e39f32d73d
anthony <anthonyalanbryson@gmail.com>
2023-11-20 18:29:53 -0700

    second phase

--- starterCode/js/main.js
+++ starterCode/js/main.js
@@ -2,24 +2,29 @@
 
 // So we don't have to keep re-finding things on page, find DOM elements once:
 //practice making change and committing 
-
 const $body = $("body");
-
-const $storiesLoadingMsg = $("#stories-loading-msg");
-const $allStoriesList = $("#all-stories-list");
-
-const $loginForm = $("#login-form");
-const $signupForm = $("#signup-form");
-
+//div[1] a[0]
 const $navLogin = $("#nav-login");
+//div[1] a[1]
 const $navUserProfile = $("#nav-user-profile");
+//div[1] a[2]
+const $storiesContainer = $(".stories-container")
+const $userProfile = $("#user-profile");
+
 const $navLogOut = $("#nav-logout");
+//div3
+const $storiesLoadingMsg = $("#stories-loading-msg");
+//div 3 ol
+const $allStoriesList = $("#all-stories-list");
+//form
+const $loginForm = $("#login-form");
+//form2
+const $signupForm = $("#signup-form");
 
 /** To make it easier for individual components to show just themselves, this
  * is a useful function that hides pretty much everything on the page. After
  * calling this, individual components can re-show just what they want.
  */
-
 function hidePageComponents() {
   const components = [
     $allStoriesList,
@@ -28,24 +33,25 @@ function hidePageComponents() {
   ];
   components.forEach(c => c.hide());
 }
-
 /** Overall function to kick off the app. */
-
 async function start() {
   console.debug("start");
-
   // "Remember logged-in user" and log in, if credentials in localStorage
+   //-in users.js; use user credentials in local storage
   await checkForRememberedUser();
+  //-in stories.js; get and show stories when first loads
   await getAndShowStoriesOnStart();
-
+//--currentUswer is in user.js; let currentUswer;
+  //--when user signs up, set up the ui; shows stories list; update navbar;
+  //--check whether var currentUser is truthy; so checks to see if variable has assigned value?;
+  //--if there is a logged in user, set up the ui; show stories list; update navbar, etc.
   // if we got a logged-in user
   if (currentUser) updateUIOnUserLogin();
 }
-
 // Once the DOM is entirely loaded, begin the app
-
 console.warn("HEY STUDENT: This program sends many debug messages to" +
   " the console. If you don't see the message 'start' below this, you're not" +
   " seeing those helpful debug messages. In your browser console, click on" +
   " menu 'Default Levels' and add Verbose");
+//--function call?
 $(start);
--- starterCode/js/models.js
+++ starterCode/js/models.js
@@ -5,9 +5,8 @@ const BASE_URL = "https://hack-or-snooze-v3.herokuapp.com";
 /******************************************************************************
  * Story: a single story in the system
  */
-
+//--see next class for creating a new StoryList with Story objects
 class Story {
-
   /** Make instance of Story from data object about story:
    *   - {title, author, url, username, storyId, createdAt}
    */
@@ -20,12 +19,18 @@ class Story {
     this.username = username;
     this.createdAt = createdAt;
   }
-
   /** Parses hostname out of URL and returns it. */
 
   getHostName() {
     // UNIMPLEMENTED: complete this function!
-    return "hostname.com";
+    const urlObject = new URL (this.url);
+    console.log(urlObject);
+    let hostName = urlObject.hostname
+    console.log(hostName)
+    return hostName
+    //--their original
+    //-- return "hostname.com";
+    
   }
 }
 
@@ -33,20 +38,19 @@ class Story {
 /******************************************************************************
  * List of Story instances: used by UI to show story lists in DOM.
  */
-
+//--has getStories() and addStory() methods
 class StoryList {
   constructor(stories) {
     this.stories = stories;
   }
 
   /** Generate a new StoryList. It:
-   *
    *  - calls the API
    *  - builds an array of Story instances
    *  - makes a single StoryList instance out of that
    *  - returns the StoryList instance.
    */
-
+//--result is a new StoryList object that has as an attribute the newStoryObjectInstancesArray???; so StoryList object now has new property: an array of story objects;
   static async getStories() {
     // Note presence of `static` keyword: this indicates that getStories is
     //  **not** an instance method. Rather, it is a method that is called on the
@@ -54,16 +58,22 @@ class StoryList {
     //  instance method?
 
     // query the /stories endpoint (no auth required)
+    //--returns object with data.stories property which has an array of individual story objects?
     const response = await axios({
       url: `${BASE_URL}/stories`,
       method: "GET",
     });
+    console.log(response)
 
     // turn plain old story objects from API into instances of Story class
-    const stories = response.data.stories.map(story => new Story(story));
-
+    //--use .map to iterate over stories array;response.data.stories is an array;
+    //--a story object is the value of each iteration; creates new array with a 'new Story' object for each story in array;
+    const arrayOfNewStoryObjects = response.data.stories.map(story => new Story(story));
+    //--each Story object has author, createdAt, storyId, title, url, usename
+    console.log(arrayOfNewStoryObjects)
+ //--should now have array of 'new Story' objects
     // build an instance of our own class using the new array of stories
-    return new StoryList(stories);
+    return new StoryList(arrayOfNewStoryObjects);
   }
 
   /** Adds story data to API, makes a Story instance, adds it to story list.
@@ -82,13 +92,13 @@ class StoryList {
 /******************************************************************************
  * User: a user in the system (only used to represent the current user)
  */
-
+//--class for user object; has the methods signup(...), login(....) and loginViaStoredCredentials(token, username)
 class User {
   /** Make user instance from obj of user data and a token:
    *   - {username, name, createdAt, favorites[], ownStories[]}
    *   - token
    */
-
+ //--will pass in favorites array; see use of .map below to create that array; pass in own stories array; see use of .map below;
   constructor({
                 username,
                 name,
@@ -102,29 +112,34 @@ class User {
     this.createdAt = createdAt;
 
     // instantiate Story instances for the user's favorites and ownStories
+    //--create new Story instance for each value in favorites array
     this.favorites = favorites.map(s => new Story(s));
+    //--create new story instance for each value in array of user's own stories;
     this.ownStories = ownStories.map(s => new Story(s));
 
     // store the login token on the user so it's easy to find for API calls.
     this.loginToken = token;
   }
-
   /** Register new user in API, make User instance & return it.
    *
    * - username: a new username
    * - password: a new password
    * - name: the user's full name
    */
-
+//--register new user in api; will call this method in function for filling out form and then submit event; signup(evt)
+//--creates new User object (see return below)
   static async signup(username, password, name) {
     const response = await axios({
       url: `${BASE_URL}/signup`,
       method: "POST",
       data: { user: { username, password, name } },
     });
-
+    console.log(response)
     let { user } = response.data
-
+    //--user object has these properties: createdAt, favorites, name, stories, username
+    console.log(user)
+//--create a 'new User' object instance based on user object received in API response;
+//--see above - class Uswer - for constructor function and properties that need to be passed in
     return new User(
       {
         username: user.username,
@@ -133,6 +148,7 @@ class User {
         favorites: user.favorites,
         ownStories: user.stories
       },
+      //--apparently token isn't in user object but is data property
       response.data.token
     );
   }
@@ -149,9 +165,12 @@ class User {
       method: "POST",
       data: { user: { username, password } },
     });
+    console.log(response)
 
     let { user } = response.data;
-
+    console.log(user)
+//--so do we create another new User object based on the data in the response?
+//--like above, instantiate new User object 
     return new User(
       {
         username: user.username,
